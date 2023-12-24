4c7a56fdcc742b8e17fc96af51d501771c859689
igor-mint <i.s.kazakov89@gmail.com>
2023-12-24 19:45:43 +0300

    hw_22.1

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Version
 
 
 # Register your models here.
@@ -16,3 +16,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'version_number', 'name', 'is_active_version',)
+    list_filter = ('product', 'version_number', 'name', 'is_active_version',)
+    search_fields = ('product', 'version_number', 'name',)
+
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import ContactsView, ProductListView, ProductDetailView
+from catalog.views import ContactsView, ProductListView, ProductDetailView, ProductCreateView
 
 app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
 ]
