ee729c33d8c5a754f945159dd9eec1d4f5ab306e
igor-mint <i.s.kazakov89@gmail.com>
2024-01-22 22:26:55 +0300

    hw_23.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, never_cache
 
 from catalog.apps import CatalogConfig
 from catalog.views import ContactsView, ProductListView, ProductDetailView, ProductCreateView, ProductDeleteView, \
@@ -11,7 +11,7 @@ urlpatterns = [
     path('categories/', CategoriesListView.as_view(), name='categories'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product'),
-    path('create/', ProductCreateView.as_view(), name='product_create'),
-    path('update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
-    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('create/', never_cache(ProductCreateView.as_view()), name='product_create'),
+    path('update/<int:pk>/', never_cache(ProductUpdateView.as_view()), name='product_update'),
+    path('delete/<int:pk>/', never_cache(ProductDeleteView.as_view()), name='product_delete'),
 ]
